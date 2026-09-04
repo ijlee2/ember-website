c49c70c6312d53317776259d3b9597bd2c83cb00
ijlee2 <ijlee2@users.noreply.github.com>
2026-09-04 14:02:07 +0200

    chore: Replaced ember-fetch with native fetch

--- app/routes/releases/canary.ts
+++ app/routes/releases/canary.ts
@@ -2,8 +2,6 @@ import Route from '@ember/routing/route';
 import { type Registry as Services, service } from '@ember/service';
 import type Project from 'ember-website/models/project';
 // @ts-expect-error: Incorrect type
-import fetch from 'fetch';
-// @ts-expect-error: Incorrect type
 import { hash } from 'rsvp';
 
 type CanaryInfo = {
@@ -30,7 +28,6 @@ export default class ReleasesCanaryRoute extends Route {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
       canaryInfo: fetch(
         'https://s3.amazonaws.com/builds.emberjs.com/canary.json',
-        // @ts-expect-error: Incorrect type
         // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-return
       ).then((response) => response.json()),
     });
